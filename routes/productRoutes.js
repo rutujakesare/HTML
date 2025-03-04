1856791b48512e64c0d60cc099bc4edfd635c56d
rutujakesare <rutujakesare38@gmail.com>
2025-03-05 00:38:41 +0530

    GET request with HTML-2

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-// GET request to serve products.html
+// GET request to serve productForm.html
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/products.html"));
+    res.sendFile(path.join(__dirname, "../views/productForm.html"));
 });
 
 module.exports = router;
+
